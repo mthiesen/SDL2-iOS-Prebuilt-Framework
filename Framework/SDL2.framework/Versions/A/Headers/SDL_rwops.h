e8d519caf32f31f3daec1533b8be783e78ecca86
Malte Thiesen <malte@kamalook.de>
2016-12-04 17:09:05 +0100

    Added prebuilt framework for SDL version 2.0.5

--- Framework/SDL2.framework/Versions/A/Headers/SDL_rwops.h
+++ Framework/SDL2.framework/Versions/A/Headers/SDL_rwops.h
@@ -1,6 +1,6 @@
 /*
   Simple DirectMedia Layer
-  Copyright (C) 1997-2014 Sam Lantinga <slouken@libsdl.org>
+  Copyright (C) 1997-2016 Sam Lantinga <slouken@libsdl.org>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -29,6 +29,13 @@
 #ifndef _SDL_rwops_h
 #define _SDL_rwops_h
 
+#if defined(__clang__)
+#   if __has_warning("-Wdocumentation")
+#       pragma clang diagnostic push
+#       pragma clang diagnostic ignored "-Wdocumentation"
+#   endif
+#endif
+
 #include "SDL_stdinc.h"
 #include "SDL_error.h"
 
@@ -39,12 +46,12 @@ extern "C" {
 #endif
 
 /* RWops Types */
-#define SDL_RWOPS_UNKNOWN   0   /* Unknown stream type */
-#define SDL_RWOPS_WINFILE   1   /* Win32 file */
-#define SDL_RWOPS_STDFILE   2   /* Stdio file */
-#define SDL_RWOPS_JNIFILE   3   /* Android asset */
-#define SDL_RWOPS_MEMORY    4   /* Memory stream */
-#define SDL_RWOPS_MEMORY_RO 5   /* Read-Only memory stream */
+#define SDL_RWOPS_UNKNOWN   0U  /* Unknown stream type */
+#define SDL_RWOPS_WINFILE   1U  /* Win32 file */
+#define SDL_RWOPS_STDFILE   2U  /* Stdio file */
+#define SDL_RWOPS_JNIFILE   3U  /* Android asset */
+#define SDL_RWOPS_MEMORY    4U  /* Memory stream */
+#define SDL_RWOPS_MEMORY_RO 5U  /* Read-Only memory stream */
 
 /**
  * This is the read/write operation structure -- very basic.
@@ -93,7 +100,7 @@ typedef struct SDL_RWops
     Uint32 type;
     union
     {
-#if defined(ANDROID)
+#if defined(__ANDROID__)
         struct
         {
             void *fileNameRef;
@@ -220,12 +227,17 @@ extern DECLSPEC size_t SDLCALL SDL_WriteLE64(SDL_RWops * dst, Uint64 value);
 extern DECLSPEC size_t SDLCALL SDL_WriteBE64(SDL_RWops * dst, Uint64 value);
 /* @} *//* Write endian functions */
 
-
 /* Ends C function definitions when using C++ */
 #ifdef __cplusplus
 }
 #endif
 #include "close_code.h"
+
+#if defined(__clang__)
+#   if __has_warning("-Wdocumentation")
+#       pragma clang diagnostic pop
+#   endif
+#endif
 
 #endif /* _SDL_rwops_h */
 
